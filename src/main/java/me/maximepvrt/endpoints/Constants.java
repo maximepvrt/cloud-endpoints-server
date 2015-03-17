fc21cc9782880a5583715c0450d03c2c8060d4e2
maximepvrt <maximepvrt@gmail.com>
2015-03-17 02:10:01 +0100

    initial version

---
+++ src/main/java/me/maximepvrt/endpoints/Constants.java
@@ -0,0 +1,13 @@
+package me.maximepvrt.endpoints;
+
+/**
+ * Contains the client IDs and scopes for allowed clients consuming the helloworld API.
+ */
+public class Constants {
+  public static final String WEB_CLIENT_ID = "526374069175-4vv42arm0ksdr9a1lgkve6vbktfkmlvv.apps.googleusercontent.com";
+  public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
+  public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
+  public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
+
+  public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
+}
